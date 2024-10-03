3db3894ff3d3b97f81f992922c78b58421daa0cd
judyzero <judyzero@github.com>
2024-10-03 13:44:06 +0900

    Fix: lyrics original API 가사 정제

--- src/main/java/com/example/back/LyricsScraper.java
+++ src/main/java/com/example/back/LyricsScraper.java
@@ -1,13 +1,17 @@
 package com.example.back;
 
+import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 //가사 스크래핑
 @Service
 public class LyricsScraper {
+    private static final Pattern REMOVE_SECTION_PATTERN = Pattern.compile("\\[.*?\\]");
+
     public String scrapeLyrics(String lyricsUrl) {
         try {
             Document doc = Jsoup.connect(lyricsUrl)
@@ -15,7 +19,14 @@ public class LyricsScraper {
                 .timeout(10000)
                 .get();
 
-            String lyrics = doc.select("div[data-lyrics-container=true]").text();
+            Elements lyricsElements = doc.select("div[data-lyrics-container=true]");
+            lyricsElements.select("a, span, div").remove();
+
+            //정제
+            String lyrics = lyricsElements.html().replaceAll(REMOVE_SECTION_PATTERN.pattern(), "").trim();
+            lyrics = lyrics.replaceAll("(?i)(<br>\\s*){1,}", "<br>").trim();
+            lyrics = lyrics.replaceFirst("(?i)^<br\\s*/?>", "");
+
             return lyrics;
         } catch (Exception e) {
             e.printStackTrace();
